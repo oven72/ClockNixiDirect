2b1009cd7b6560469ba11e63b533779092b133f0
Соболев Павел <sobolevpa@opvf.ru>
2024-03-12 12:19:15 +0300

    +

---
+++ main.js
@@ -0,0 +1,39 @@
+﻿// Получение ссылок на элементы UI
+let connectButton = document.getElementById('connect');
+let disconnectButton = document.getElementById('disconnect');
+let terminalContainer = document.getElementById('terminal');
+let sendForm = document.getElementById('send-form');
+let inputField = document.getElementById('input');
+
+// Подключение к устройству при нажатии на кнопку Connect
+connectButton.addEventListener('click', function() {
+  connect();
+});
+
+// Отключение от устройства при нажатии на кнопку Disconnect
+disconnectButton.addEventListener('click', function() {
+  disconnect();
+});
+
+// Обработка события отправки формы
+sendForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Предотвратить отправку формы
+  send(inputField.value); // Отправить содержимое текстового поля
+  inputField.value = '';  // Обнулить текстовое поле
+  inputField.focus();     // Вернуть фокус на текстовое поле
+});
+
+// Запустить выбор Bluetooth устройства и подключиться к выбранному
+function connect() {
+  //
+}
+
+// Отключиться от подключенного устройства
+function disconnect() {
+  //
+}
+
+// Отправить данные подключенному устройству
+function send(data) {
+  //
+}
